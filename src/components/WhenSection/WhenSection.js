764c6c8b141b6a08523f1a9a70cb21b70f1bc25d
SeanLewisIRE <slewis100@gmail.com>
2021-06-08 22:03:00 +0100

    added mobile styles to where component. Started When component, updated timeline

--- src/components/WhenSection/WhenSection.js
+++ src/components/WhenSection/WhenSection.js
@@ -4,46 +4,93 @@ import './WhenSection.css'
 const WhenSection = () => {
 
     return (
-        <div id='when' className='when-container bg-white'>
+        <div id='when' className='when-container bg-white text-center'>
             <h2>When</h2>
-            <h3 className='line-break-header' >FRIDAY 27TH AUGUST</h3>
+            <h3 className='line-break-header' ><span>FRIDAY 27TH AUGUST</span></h3>
             <h4>1400 hrs Zulu</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p className="w-80 mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         
-            <h3 className='line-break-header' >SATURDAY 28TH AUGUST</h3>
+            <h3 className='line-break-header'><span>SATURDAY 28TH AUGUST</span></h3>
             <h4>1300 hrs Zulu</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p className="w-80 mx-auto" >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         
-        
-            <div className='timeline bg-gray-200'>
-                <h4 >Thursday</h4>
-                <div className="tl-container right">
-                    <div className="content">
-                        <h3>CEREMONY</h3>
-                        <p>Church</p>
-                        <p>*time*</p>
+            <h4 className="my-3">Thursday</h4>
+
+            <div class="container">
+                <div class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
+
+                    <div class="flex flex-row-reverse md:contents">
+                        <div
+                            class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+                        >
+                            <h3>CEREMONY</h3>
+                            <p>Church</p>
+                            <p>*time*</p>
+                        </div>
+                        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                            <div class="h-full w-6 flex items-center justify-center">
+                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                            </div>
+                            <div
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                            ></div>
+                        </div>
                     </div>
-                </div>
-                <div className="tl-container left">
-                    <div className="content">
-                        <h3>DRINKS RECEPTION</h3>
-                        <p>Tankardstown House</p>
+
+                    <div class="flex flex-row-reverse md:contents">
+                        <div
+                            class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+                        >
+                            <h3>DINNER</h3>
+                            <p>Tankardstown</p>
+                            <p>*Sub address if known*</p>
+                        </div>
+                        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                            <div class="h-full w-6 flex items-center justify-center">
+                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                            </div>
+                            <div
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                            ></div>
+                        </div>
                     </div>
-                </div>
-                <div className="tl-container right">
-                    <div className="content">
-                        <h3>DINNER</h3>
-                        <p>Tankardstown</p>
-                        <p>*Sub address if known*</p>
+
+                    <div class="flex flex-row-reverse md:contents">
+                        <div
+                            class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+                        >
+
+                            <h3>DRINKS RECEPTION</h3>
+                            <p>Tankardstown House</p>
+                        </div>
+                        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                            <div class="h-full w-6 flex items-center justify-center">
+                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                            </div>
+                            <div
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                            ></div>
+                        </div>
                     </div>
-                </div>
-                <h4>Friday</h4>
-                <div className="tl-container left">
-                    <div className="content">
-                        <h3>BBQ</h3>
-                        <p>Location</p>
-                        <p>*time*</p>
+
+                    <div class="flex md:contents">
+                        <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                            <div class="h-full w-6 flex items-center justify-center">
+                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                            </div>
+                            <div
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                            ></div>
+                        </div>
+                        <div
+                            class="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+                        >
+                            <h3>BBQ</h3>
+                            <p>Location</p>
+                            <p>*time*</p>
+                        </div>
                     </div>
+                
                 </div>
             </div>
 
--- src/components/WhereSection/WhereSection.js
+++ src/components/WhereSection/WhereSection.js
@@ -10,29 +10,69 @@ import './WhereSection.css'
 
 const WhereSection = () => {
     return (
-        <div className="where-container bg-white" id="where">
+        <div className="where-container bg-white text-center" id="where">
             <h2> Where...</h2>
-            <div className="ceremony-container">
-                <h3 className='line-break-header' >CEREMONY</h3>
-                <h4>RATHFEIGH CHURCH</h4>
-                <address>
-                    Church of Immaculate Conception
-                    Rathfeigh, Co. Meath.
-                </address>
-                <img src={churchPhoto} alt='Lorem Ipsum Church'/>
-                <a href='https://www.google.ie/maps/place/Church+of+the+Immaculate+Conception/@53.5909677,-6.4898685,17z/data=!3m1!4b1!4m5!3m4!1s0x4867404e43b05d75:0x3d6947500412ffde!8m2!3d53.5909727!4d-6.4876838?hl=en'><FiMapPin /> Rathfeigh Church, Google Maps </a>
-            </div>
 
-            <div className="reception-container">
-                <h3 className='line-break-header' >RECEPTION</h3>
-                <h4>TANKARDSTOWN HOUSE</h4>
-                <address>Rathkenny Slane, Co. Meath</address>
-                <img src={tankardstown} alt='tankardstown house hotel'/>
-                <a href='tel:+353419824621'><AiTwotonePhone />+353 41 982 4621</a>
-                <a href='https://www.tankardstown.ie/' target='_blank' rel='noreferrer'><CgWebsite />Tankardstown Hosue</a>
-                <a href='https://g.page/Tankardstown?share'> <FiMapPin />Tankardstown House, Google Maps</a>
-            </div>
+            <div className="flex flex-col lg:flex-row">
+                <div className="ceremony-container">
+                    <h3 className='line-break-header'><span>CEREMONY</span></h3>
+                    <h4>RATHFEIGH CHURCH</h4>
+                    <address className="grey w-80 mx-auto">
+                        Church of Immaculate Conception
+                        <strong> Rathfeigh</strong>, Co. Meath.
+                    </address>
+                    <img src={churchPhoto} alt='RATHFEIGH CHURCH' className="w-80 mx-auto my-1 border border-dashed border-light-blue-500 p-1"/>
+                    
+                    <div className="flex flex-row items-center justify-center">
+                    <FiMapPin />
+                    <a  
+                        className="px-1 my-1"
+                        href='https://www.google.ie/maps/place/Church+of+the+Immaculate+Conception/@53.5909677,-6.4898685,17z/data=!3m1!4b1!4m5!3m4!1s0x4867404e43b05d75:0x3d6947500412ffde!8m2!3d53.5909727!4d-6.4876838?hl=en'
+                        target="_blank"
+                        rel="noreferrer"
+                        >
+                        Rathfeigh Church, Google Maps 
+                    </a>
+                    </div>
+                </div>
 
+                <div className="reception-container">
+                    <h3 className='line-break-header' ><span>RECEPTION</span></h3>
+                    <h4>TANKARDSTOWN HOUSE</h4>
+                    <address className="grey w-80 mx-auto">Rathkenny<strong> Slane</strong>, Co. Meath</address>
+                    <img src={tankardstown} alt='tankardstown house hotel' className="w-80 mx-auto my-1 border border-dashed border-light-blue-500 p-1"/>
+                    
+                    <div className="flex flex-row items-center justify-center">
+                        <AiTwotonePhone />
+                        <a 
+                            className="px-1 my-1"
+                            href='tel:+353419824621'>
+                            +353 41 982 4621
+                        </a>
+                    </div>
+                    
+                    <div className="flex flex-row items-center justify-center">
+                        <CgWebsite />
+                        <a 
+                            className="px-1 my-1"
+                            href='https://www.tankardstown.ie/' target='_blank' rel='noreferrer'>
+                            Tankardstown House
+                        </a>
+                    </div>
+
+                    <div className="flex flex-row items-center justify-center">
+                        <FiMapPin />
+                        <a 
+                        className="px-1 my-1"
+                        href='https://g.page/Tankardstown?share'
+                        target="_blank"
+                        rel="noreferrer"
+                        >
+                            Tankardstown House, Google Maps
+                        </a>
+                    </div>
+                </div>
+            </div>
             
         </div>
     )
