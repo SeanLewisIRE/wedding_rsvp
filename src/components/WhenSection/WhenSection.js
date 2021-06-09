daccf48e0c5beb6d3c48f28ad23f81e752219f2d
SeanLewisIRE <slewis100@gmail.com>
2021-06-09 21:42:37 +0100

    completed timeline

--- src/components/WhenSection/WhenSection.js
+++ src/components/WhenSection/WhenSection.js
@@ -16,59 +16,23 @@ const WhenSection = () => {
         
             <h4 className="my-3">Thursday</h4>
 
-            <div class="container">
+            <div class="container w-80 mx-auto">
                 <div class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
 
                     <div class="flex flex-row-reverse md:contents">
                         <div
-                            class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+                            class="col-start-1 col-end-5 p-4 rounded-xl my-4 mr-auto text-left md:ml-auto md:mr-0 md:text-right"
                         >
                             <h3>CEREMONY</h3>
-                            <p>Church</p>
-                            <p>*time*</p>
+                            <span className="grey">Church</span>
+                            <span className="green-font block">*time*</span>
                         </div>
                         <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
                             <div class="h-full w-6 flex items-center justify-center">
-                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                                <div class="h-full w-1 gold pointer-events-none"></div>
                             </div>
                             <div
-                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
-                            ></div>
-                        </div>
-                    </div>
-
-                    <div class="flex flex-row-reverse md:contents">
-                        <div
-                            class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
-                        >
-                            <h3>DINNER</h3>
-                            <p>Tankardstown</p>
-                            <p>*Sub address if known*</p>
-                        </div>
-                        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-                            <div class="h-full w-6 flex items-center justify-center">
-                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
-                            </div>
-                            <div
-                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
-                            ></div>
-                        </div>
-                    </div>
-
-                    <div class="flex flex-row-reverse md:contents">
-                        <div
-                            class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
-                        >
-
-                            <h3>DRINKS RECEPTION</h3>
-                            <p>Tankardstown House</p>
-                        </div>
-                        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-                            <div class="h-full w-6 flex items-center justify-center">
-                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
-                            </div>
-                            <div
-                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full gold"
                             ></div>
                         </div>
                     </div>
@@ -76,20 +40,62 @@ const WhenSection = () => {
                     <div class="flex md:contents">
                         <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
                             <div class="h-full w-6 flex items-center justify-center">
-                                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                                <div class="h-full w-1 gold pointer-events-none"></div>
                             </div>
                             <div
-                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full gold "
                             ></div>
                         </div>
                         <div
-                            class="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+                            class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto text-left"
                         >
                             <h3>BBQ</h3>
-                            <p>Location</p>
-                            <p>*time*</p>
+                            <span className="grey">Location</span>
+                            <span className="green-font block">*time*</span>
                         </div>
                     </div>
+
+                    <div class="flex flex-row-reverse md:contents">
+                        <div
+                            class="col-start-1 col-end-5 p-4 rounded-xl my-4 mr-auto text-left md:ml-auto md:mr-0 md:text-right"
+                        >
+                            <h3>DINNER</h3>
+                            <span className="grey">Tankardstown</span>
+                            <span className="grey block">*Sub address if known*</span>
+                        </div>
+                        <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                            <div class="h-full w-6 flex items-center justify-center">
+                                <div class="h-full w-1 gold pointer-events-none"></div>
+                            </div>
+                            <div
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full gold "
+                            ></div>
+                        </div>
+                    </div>
+                </div>
+
+                <h4 className="my-3">Thursday</h4>
+                
+                <div class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
+
+                    <div class="flex md:contents">
+                        <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                            <div class="h-full w-6 flex items-center justify-center">
+                                <div class="h-full w-1 gold pointer-events-none"></div>
+                            </div>
+                            <div
+                                class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full gold "
+                            ></div>
+                        </div>
+                        <div
+                            class="col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto text-left"
+                        >
+                            <h3>BBQ</h3>
+                            <span className="grey">Location</span>
+                            <span className="green-font block">*time*</span>
+                        </div>
+                    </div>
+
                 
                 </div>
             </div>
