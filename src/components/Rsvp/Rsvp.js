696e6e6d981d0b82f9fb5423330b647593ec025c
SeanLewisIRE <slewis100@gmail.com>
2021-06-26 11:48:40 +0100

    Added transition to welcome page

--- src/components/Rsvp/Rsvp.js
+++ src/components/Rsvp/Rsvp.js
@@ -3,7 +3,8 @@ import emailjs from 'emailjs-com';
 const Rsvp = () => {
 
     const [numberOfGuests, setNumberOfGuests] = useState(false)
-
+    const [successState, SetSuccessState] = useState(false)
+    const [failState, SetFailState] = useState(false)
 
     const handleChange = (e) => {
         if (e.target.value > 1 ) {
@@ -16,16 +17,24 @@ const Rsvp = () => {
     function sendEmail(e) {
         e.preventDefault();
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        emailjs.sendForm('service_chncrll', 'template_0es0cno', e.target, 'user_jH5zV1Y5792XWvBUxYxDk') //
             .then((result) => {
-                console.log(result.text);
+                if (result.status === 200) {
+                    console.log(result);
+                    SetSuccessState(true)
+                } else {
+                    console.log(result);
+                    SetFailState(true)
+                }
+
             }, (error) => {
-                console.log(error.text);
+                SetFailState(true)
             });
     }
 
     return (
         <div className='rsvp-container bg-white ' id="rsvp">
+
             <h2 className="text-center">RSVP</h2>
 
             <div className="text-center flex flex-col">
@@ -36,7 +45,7 @@ const Rsvp = () => {
             <form className="contact-form flex flex-col w-4/5 md:w-3/5 lg:w-2/5 mx-auto" onSubmit={sendEmail}>
                 
                 <label htmlFor="name">Name{numberOfGuests ? "s" : ""}</label>
-                <input className="border border-dashed border-light-blue-500" type="text" name="name" />
+                <input className="border border-dashed border-light-blue-500" type="text" name="name" required/>
 
                 <label className="mt-3" htmlFor="email">Email</label>
                 <input 
@@ -50,7 +59,7 @@ const Rsvp = () => {
                 </select>
 
                 <div className="flex items-center my-2">
-                    <input type="radio" id="attend_one" name="attend_radio" value="I/We will attend on Saturday 25th August." />
+                    <input type="radio" id="attend_one" name="attend_radio" value="I/We will attend on Saturday 25th August." required/>
                     <label className="px-2" htmlFor="attend_one">{numberOfGuests ? "We" : "I"} will attend on Saturday 25th August.</label>
                 </div>
 
@@ -68,11 +77,41 @@ const Rsvp = () => {
                 <label className="my-1">Message</label>
                 <textarea 
                     className="border border-dashed border-light-blue-500"
-                    name="dietary_message" />
-                <input 
+                    name="dietary_message" 
+                />
+
+                {successState &&
+                    <div className="flex justify-center items-center mt-4 m-1 font-medium py-1 px-2 bg-white rounded-md text-green-700 bg-green-100 border border-green-300 ">
+                        <div slot="avatar">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check-circle w-5 h-5 mx-2">
+                                <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                                <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                            </svg>
+                        </div>
+                        <div className="text-xl font-normal  max-w-full flex-initial">
+                            RSVP Sent Sucessfully    
+                        </div>
+                    </div>
+                }
+
+                {failState &&
+                    <div role="alert" className="mt-4">
+                        <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                            There was a problem sending the RSVP at this time
+                        </div>
+                        <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                            <p>
+                                Please email <a href="mailto:sarahanddonnchadh@gmail.com" target="_blank" rel="noreferrer">sarahanddonnchadh@gmail.com</a> with your response
+                            </p>
+                        </div>
+                    </div>
+                }
+
+                <input
                     className="gold my-4 p-3 w-8/12 mx-auto rounded-md"
                     type="submit" 
                     value="SEND" 
+                    disabled={successState}
                 />
             </form>
         </div>
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import dcPhoto from "../../static/photos/donnchadh.jpg"
 import slPhoto from "../../static/photos/sarah.jpg"
 import './Welcome.css'
 
 const Welcome = () => {
+
+    const [isVisible, setVisible] = React.useState(true);
+    const domRef = React.useRef();
+    
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => setVisible(entry.isIntersecting));
+        });
+        observer.observe(domRef.current);
+        return () => observer.unobserve(domRef.current);
+    })
+
     return (
         <div className="text-center bg-white">
             
             <h2>Welcome!</h2>
             
             <div className="image-container flex items-center justify-center my-6">
-                <div className="w-4/12 md:w-3/12 lg:w-48">
+                <div className={`fade-in-section ${isVisible ? 'is-visible' : ''} left w-4/12 md:w-3/12 lg:w-48`} ref={domRef}>
                     <img className="rounded-full border border-dashed border-light-blue-500 p-1" src={dcPhoto} alt="The groom" />
                     <span className="photo-name italic font-medium">Donnchadh</span>
                 </div>
                 
                 <h2 className="p-0 mx-2 md:mx-4 lg:mx-8">&#38;</h2>
 
-                <div className="w-4/12 md:w-3/12 lg:w-48">
+                <div className={`fade-in-section ${isVisible ? 'is-visible' : ''} right w-4/12 md:w-3/12 lg:w-48`} ref={domRef}>
                     <img className="rounded-full border border-dashed border-light-blue-500 p-1" src={slPhoto} alt="The bride" />
                     <span className="photo-name italic font-medium">Sarah</span>
                 </div>
